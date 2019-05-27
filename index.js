1ed095f27ee77b5481387bb19927fa69e94137fb
Tri Nguyen <zymethyang@gmail.com>
2019-05-28 01:09:59 +0700

    change ver4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
-app.get('/', (req, res) => res.send({ status: false, version: 2 }))
+app.get('/', (req, res) => res.send({ status: true, version: 4 }))
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
